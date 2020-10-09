1cd88f58bdc2e99ad00f8fdd567c9a5aa5d0fc8f
benjamimWalker <bhaskdelta2@gmail.com>
2020-10-09 10:22:11 -0300

    Criação da classe do tempo mundial, logo após a implementação da lógica em uma classe secundária

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -11,5 +14,23 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: SafeArea(child: Text('Loading Screen')),
     );
+  }
+
+  getTime() async {
+    Response response =
+        await get("http://worldtimeapi.org/api/timezone/Europe/London");
+    var myMap = jsonDecode(response.body);
+
+    //get properties from data
+    var dateTime = myMap['datetime'];
+    String offset = myMap['utc_offset'].substring(1);
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
   }
 }
